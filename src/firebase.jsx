8f554a99c7173b06680389a90acc6e430dacd97e
Siddharth Godambe <157814736+Sid080802@users.noreply.github.com>
2025-01-09 18:03:21 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,12 +6,12 @@ import 'firebase/compat/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDJwhGc73oZY4u90ZhC8r5ftZGSEgPaFEU",
-  authDomain: "mobile-authentication-5d93f.firebaseapp.com",
-  projectId: "mobile-authentication-5d93f",
-  storageBucket: "mobile-authentication-5d93f.firebasestorage.app",
-  messagingSenderId: "721450680020",
-  appId: "1:721450680020:web:1483d95d36a34cc2b8c541"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 if (!firebase.apps.length) {
